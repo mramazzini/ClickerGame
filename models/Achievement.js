f7e952678501b57b0431471782353d240e54d386
mramazzini <mramazzini123@gmail.com>
2022-10-14 10:36:27 -0500

    Added Achievement model

---
+++ models/Achievement.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const { model } = require('../config/connection');
+const sequelize = require('../config/connection');
+
+class Achievement extends Model {}
+
+Achievement.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title:{
+            type: DataTypes.STRING,
+            allowNull:false
+        },
+        flavor_text:{
+            type: DataTypes.STRING,
+            allowNull:false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'user',
+              key: 'id'
+            },
+          },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'achievement',
+      }
+)
+
+model.exports = Achievement;
