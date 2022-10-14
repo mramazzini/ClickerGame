e2cf383d42366a544e06c692a883997ede3bf687
mramazzini <mramazzini123@gmail.com>
2022-10-14 10:42:18 -0500

    Added index.js

--- models/index.js
+++ models/index.js
@@ -1,2 +1,16 @@
 const User = require('./User');
-const Flux = require('./Flux');
+const Achievement = require('./Achievement');
+const GameData = require('./GameData');
+
+
+
+Achievement.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+GameData.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+User.HasMany(Achievement, {
+    foreignKey: 'user_id'
+})
+
